2db87f245a3721ddc7946fa4336916fc0ec0b1ad
anasabdelhakim <117493389+anasabdelhakim@users.noreply.github.com>
2024-11-05 07:52:55 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,21 @@
-
 // Selectors
 const toggleBtn = document.querySelector(".theme-switch__checkbox");
 const bodyElement = document.body;
-const links = document.querySelectorAll("a");
 const header = document.querySelector("header");
-const Textarea = document.querySelector("textarea");
 const navbar = document.querySelector(".navbar-nav");
-const cards = document.querySelectorAll(".card");
-const sectionTitles = document.querySelectorAll(".Section-title");
-const footer = document.querySelector(".footer-preference");
-const contactInputs = document.querySelectorAll(
-  ".Contact-us-form-inputs input"
-);
-
-// Set styles for light theme
-function setLightTheme() {
-  updateLinkStyles("black");
-  header.classList.remove("headerBackground");
-  updateCardStyles(false);
-  updateSectionTitleStyles("white");
-  footer.style.backgroundColor = "white";
-  updateInputStyles("black", false);
-}
-
-// Set styles for dark theme
-function setDarkTheme() {
-  updateLinkStyles("white");
-  header.classList.add("headerBackground");
-  updateCardStyles(true);
-  updateSectionTitleStyles("#02050c");
-  footer.style.backgroundColor = "#02050c";
-  updateInputStyles("white", true);
-}
-
-// Update link styles
-function updateLinkStyles(color) {
-  links.forEach((link) => (link.style.color = color));
-}
-
-// Update card styles
-function updateCardStyles(isDark) {
-  cards.forEach((card) => {
-    if (isDark) {
-      card.classList.add("cardBackground");
-    } else {
-      card.classList.remove("cardBackground");
-    }
-  });
-}
-
-// Update section title styles
-function updateSectionTitleStyles(color) {
-  sectionTitles.forEach((section) => {
-    section.style.backgroundColor = color;
-  });
-}
-
-// Update input styles
-function updateInputStyles(color, isDark) {
-  contactInputs.forEach((input) => {
-    input.classList.toggle("headerBackground", isDark);
-    input.style.color = color;
-  });
-  Textarea.classList.toggle("headerBackground", isDark);
-  text.style.color = color;
-}
+const scrollToTopButton = document.getElementById("home");
 
 // Toggle theme function
 function toggleTheme() {
-  bodyElement.classList.toggle("dark-mode");
+  bodyElement.classList.toggle("darkmode");
   toggleBtn.checked ? setDarkTheme() : setLightTheme();
 }
+toggleBtn.checked = false;
+
 // Event listener
 toggleBtn.addEventListener("change", toggleTheme);
 
 // Default values
-toggleBtn.checked = false;
 let prevScrollPos = 0;
 let isScrollListenerAdded = false;
 
@@ -94,7 +34,6 @@ const handleScroll = () => {
 };
 //Scroll Up
 function scrollToTopOfHeader() {
-  const header = document.querySelector("header");
   const headerPosition = header.getBoundingClientRect().top;
   window.scrollTo({
     top: headerPosition,
@@ -105,14 +44,12 @@ function scrollToTopOfHeader() {
 // Show/Hide border on header based on scroll position
 const ShowenBorderHeader = () => {
   const currentScrollPos = window.scrollY;
-
   if (currentScrollPos === 0) {
     header.classList.remove("removeBorder");
   } else if (!header.classList.contains("removeBorder")) {
     header.classList.add("removeBorder");
   }
 };
-const scrollToTopButton = document.getElementById("home");
 if (scrollToTopButton) {
   scrollToTopButton.addEventListener("click", scrollToTopOfHeader);
 }
